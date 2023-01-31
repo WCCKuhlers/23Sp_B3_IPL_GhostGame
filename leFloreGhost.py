0ab1b09770f35238b99b52ac45ef0288612f8320
lunagiri <124083258+lunagiri@users.noreply.github.com>
2023-01-31 12:21:55 -0600

    Add files via upload

---
+++ leFloreGhost.py
@@ -0,0 +1,16 @@
+from random import randint
+print ("deez nuts")
+start_game = True #sentinel value
+score = 0
+
+while start_game:
+    ghsot door = randint (1,3)
+    print("There is nut ahead")
+    print("nut is behind one of those doors")
+    print("pick a door, enter 1, 2, or 3")
+    user_door_num = int(user_door)
+    start_game = false
+    print("the nut door is: ", Ghost_door)
+    print("The players door is: ", user_door_num)
+    
+    
